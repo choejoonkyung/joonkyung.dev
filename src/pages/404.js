6d2510d5b2b5d560d3a87a0733efaed2caf83aec
choejoonkyung <choejunkyung@gmail.com>
2022-04-24 02:27:02 +0900

    404 페이지

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,8 +12,8 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <Head title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>찾을 수 없음!</h1>
+        <p>음... 요청하신 페이지를 찾을 수 없습니다. </p>
       </Layout>
     )
   }
